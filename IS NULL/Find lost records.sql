7ab305bfdf471580d63c56ba3274765d1f69a594
Park's coding <44426450+gocyzhod@users.noreply.github.com>
2021-08-23 17:06:27 +0900

    없어진 기록 찾기_LV3

---
+++ IS NULL/Find lost records.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT outs.animal_id, outs.name
+FROM animal_outs outs
+    LEFT JOIN animal_ins ins
+    ON outs.animal_id = ins.animal_id
+WHERE ins.animal_id IS NULL
+ORDER BY outs.animal_id
